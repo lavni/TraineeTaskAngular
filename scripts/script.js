a8b7fa9d763d1fe0fcec7a447b5673c387ecab6f
Inna Avramenko <InnaAvramenko@coherentsolutions.com>
2015-05-25 17:44:33 +0300

    added popup with address

--- scripts/script.js
+++ scripts/script.js
@@ -6,79 +6,87 @@
 
 var myAppModule = angular.module('MyApp', []);
 
+myAppModule.factory('Persons', function() {
+    var persons = {};
+    persons.query = function() {
+        return [
+            {
+                "firstName": "John",
+                "lastName": "Smith",
+                "age": 25,
+                "address":
+                {
+                    "streetAddress": "21 2nd Street",
+                    "city": "New York",
+                    "state": "NY",
+                    "postalCode": "10021"
+                },
+                "phoneNumber":
+                    [
+                        {
+                            "type": "home",
+                            "number": "212 555-1234"
+                        },
+                        {
+                            "type": "fax",
+                            "number": "646 555-4567"
+                        }
+                    ]
+            },
+            {
+                "firstName": "Simona",
+                "lastName": "Morasca",
+                "age": 22,
+                "address":
+                {
+                    "streetAddress": "3 Mcauley Dr",
+                    "city": "Ashland",
+                    "state": "OH",
+                    "postalCode": "44805"
+                },
+                "phoneNumber":
+                    [
+                        {
+                            "type": "home",
+                            "number": "419-503-2484"
+                        },
+                        {
+                            "type": "fax",
+                            "number": "419-800-6759"
+                        }
+                    ]
+            },
+            {
+                "firstName": "Josephine",
+                "lastName": "Darakjy",
+                "age": 33,
+                "address":
+                {
+                    "streetAddress": "4 B Blue Ridge Blvd",
+                    "city": "Brighton",
+                    "state": "MI",
+                    "postalCode": "48116"
+                },
+                "phoneNumber":
+                    [
+                        {
+                            "type": "home",
+                            "number": "973-605-6492"
+                        },
+                        {
+                            "type": "fax",
+                            "number": "602-919-4211"
+                        }
+                    ]
+            }
+        ];
+    };
+    return persons;
+});
+
 myAppModule.controller('PersonController',
-    function($scope) {
-        $scope.persons = [
-        {
-            "firstName": "John",
-            "lastName": "Smith",
-            "age": 25,
-            "address":
-            {
-                "streetAddress": "21 2nd Street",
-                "city": "New York",
-                "state": "NY",
-                "postalCode": "10021"
-            },
-            "phoneNumber":
-                [
-                    {
-                        "type": "home",
-                        "number": "212 555-1234"
-                    },
-                    {
-                        "type": "fax",
-                        "number": "646 555-4567"
-                    }
-                ]
-        },
-        {
-            "firstName": "Simona",
-            "lastName": "Morasca",
-            "age": 22,
-            "address":
-            {
-                "streetAddress": "3 Mcauley Dr",
-                "city": "Ashland",
-                "state": "OH",
-                "postalCode": "44805"
-            },
-            "phoneNumber":
-                [
-                    {
-                        "type": "home",
-                        "number": "419-503-2484"
-                    },
-                    {
-                        "type": "fax",
-                        "number": "419-800-6759"
-                    }
-                ]
-        },
-        {
-            "firstName": "Josephine",
-            "lastName": "Darakjy",
-            "age": 33,
-            "address":
-            {
-                "streetAddress": "4 B Blue Ridge Blvd",
-                "city": "Brighton",
-                "state": "MI",
-                "postalCode": "48116"
-            },
-            "phoneNumber":
-                [
-                    {
-                        "type": "home",
-                        "number": "973-605-6492"
-                    },
-                    {
-                        "type": "fax",
-                        "number": "602-919-4211"
-                    }
-                ]
-        }
-    ];
+    function(Persons, $scope) {
+        $scope.persons = Persons.query();
 
         $scope.hide = false;
         $scope.toggleContent = function(){
@@ -126,6 +134,16 @@ myAppModule.controller('PersonController',
             });
            return $scope.n;
         }
+
+        $scope.showLast = false;
+        $scope.openInfo = function(index){
+            $scope.person = $scope.persons[index];
+            $scope.showLast = false;
+        }
+        $scope.save = function(){
+            $scope.showLast = true;
+        }
+
  });
 
 
